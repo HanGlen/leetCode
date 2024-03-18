a73d91efa0c205672754aed0b6638a0f1a64976d
HanGlen <38401070+HanGlen@users.noreply.github.com>
2024-03-18 16:29:50 +0800

    283. 移动零

---
+++ leetcode/editor/cn/MoveZeroes.java
@@ -0,0 +1,102 @@
+package leetcode.editor.cn;
+
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。 
+//
+// 请注意 ，必须在不复制数组的情况下原地对数组进行操作。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [0,1,0,3,12]
+//输出: [1,3,12,0,0]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: nums = [0]
+//输出: [0] 
+//
+// 
+//
+// 提示: 
+// 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -2³¹ <= nums[i] <= 2³¹ - 1 
+// 
+//
+// 
+//
+// 进阶：你能尽量减少完成的操作次数吗？ 
+//
+// Related Topics 数组 双指针 👍 2317 👎 0
+
+public class MoveZeroes {
+    public static void main(String[] args) {
+        Solution solution = new MoveZeroes().new Solution();
+        int[] nums = {0, 1, 0, 3, 12};
+        solution.moveZeroes(nums);
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void moveZeroes(int[] nums) {
+//            int zero, num = 0;
+//            boolean stop = true;
+//            for (int i = 0; i < nums.length - 1; i++) {
+//
+//                if (nums[i] == 0) {
+//                    zero = i;
+//                    int a = nums[zero];
+//
+//                    while (stop) {
+//
+//                        if (num == nums.length) {
+//                            break;
+//                        }
+//                        if (nums[num] == 0) {
+//                            num += 1;
+//
+//                        } else {
+//                            stop = false;
+//                        }
+//
+//                    }
+//                    int b = nums[num];
+//
+//                    nums[zero] = b;
+//                    nums[num] = a;
+//
+//                }
+//            }
+            boolean stop = true;
+            int i = 0;
+            while (i < nums.length && stop) {
+                if (nums[i] == 0) {
+                    for (int j = i; j < nums.length; j++) {
+                        if (nums[j] != 0) {
+                            int k = nums[i];
+                            nums[i] = nums[j];
+                            nums[j] = k;
+                            break;
+                        }
+                        if (j == nums.length - 1) {
+                            stop = false;
+                            break;
+                        }
+                    }
+                }
+                i++;
+
+            }
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
